4c3113101e6e19b1586555d79c4bce4ee2e38319
VTU28948-G-Sudheep <vtu28948@veltech.edu.in>
2026-09-18 10:15:02 +0530

    Implement palindrome index checker

---
+++ W6_PalindromeIndex.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Solution {
+
+    static int palindromeIndex(String s) {
+        int l = 0, r = s.length() - 1;
+
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)) {
+                if (check(s, l + 1, r))
+                    return l;
+                if (check(s, l, r - 1))
+                    return r;
+                return -1;
+            }
+            l++;
+            r--;
+        }
+        return -1;
+    }
+
+    static boolean check(String s, int l, int r) {
+        while (l < r) {
+            if (s.charAt(l++) != s.charAt(r--))
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int q = sc.nextInt();
+
+        while (q-- > 0)
+            System.out.println(palindromeIndex(sc.next()));
+    }
+}
